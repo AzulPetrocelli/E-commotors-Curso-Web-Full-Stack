03be5fd28381b0bff6706e6e6fec42c9869b1f4e
AzulPetrocelli <azulpetrocelli81@gmail.com>
2024-12-23 00:20:47 -0300

    Ultimos retoques al main

--- E-commotors/resources/views/header.blade.php
+++ E-commotors/resources/views/header.blade.php
@@ -1,38 +1,64 @@
-<header>
-    <nav class="navbar navbar-expand-sm position-fixed costum-navbar">
-        <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarScroll">
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-                <img src="images/logo.png" alt="Logo" width="60px">
-                <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
-                  <li class="nav-item">
-                    <a class="nav-link jaro" href="#">Motos</a>
-                  </li>
-                  <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle jaro" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Accesorios
-                    </a>
-                    <ul class="dropdown-menu">
-                      <!--Agregar foreach-->
-                      <li><a class="dropdown-item jaro" href="#">Puños</a></li>
-                      <li><a class="dropdown-item jaro" href="#">Another action</a></li>
-                    </ul>
-                  </li>
-                  <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle jaro" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Repuestos
-                    </a>
-                    <ul class="dropdown-menu">
-                      <!--Agregar foreach-->
-                      <li><a class="dropdown-item jaro" href="#">Piñom</a></li>
-                      <li><a class="dropdown-item jaro" href="#">Another action</a></li>
-                    </ul>
-                  </li>
-                </ul>
-              </div>
-        </div>
-    </nav>
-</header>
+    <!--Estilos Boostrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+    
+    <!--Tipografia-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jaro:opsz@6..72&family=Varela+Round&display=swap" rel="stylesheet">
+    
+    <!--ICONOS-->
+    <link rel= "stylesheet" href= "https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css" >
+    
+    <!--Estilos CSS-->
+    <link rel="stylesheet" href="css/main.css">
+
+    <title>Document</title>
+
+</head>
+
+<body>
+  <header>
+      <nav class="navbar navbar-expand-sm position-fixed costum-navbar shadow-lg">
+          <div class="container-fluid">
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarScroll">
+
+                  <img src="images/logo.png" alt="Logo" width="60px">
+                  <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+                    <li class="nav-item">
+                      <a class="nav-link jaro" href="#">Motos</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle jaro" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Accesorios
+                      </a>
+                      <ul class="dropdown-menu">
+                        <!--Agregar foreach-->
+                        <li><a class="dropdown-item jaro" href="#">Puños</a></li>
+                        <li><a class="dropdown-item jaro" href="#">Another action</a></li>
+                      </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle jaro" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Repuestos
+                      </a>
+                      <ul class="dropdown-menu">
+                        <!--Agregar foreach-->
+                        <li><a class="dropdown-item jaro" href="#">Piñom</a></li>
+                        <li><a class="dropdown-item jaro" href="#">Another action</a></li>
+                      </ul>
+                    </li>
+                  </ul>
+                </div>
+          </div>
+      </nav>
+  </header>
--- E-commotors/resources/views/inicio.blade.php
+++ E-commotors/resources/views/inicio.blade.php
@@ -1,29 +1,3 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!--Estilos Boostrap-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
-    
-    <!--Tipografia-->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Jaro:opsz@6..72&family=Varela+Round&display=swap" rel="stylesheet">
-    
-    <!--ICONOS-->
-    <link rel= "stylesheet" href= "https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css" >
-    
-    <!--Estilos CSS-->
-    <link rel="stylesheet" href="css/main.css">
-
-    <title>Document</title>
-
-</head>
-
-<body>
     <!--HEADER-->
     @include('header')
 
@@ -50,44 +24,11 @@
 
     <!--LISTADO DE PRODUCTOS-->
 
-    <section class="container-productos">
+    <section class="container-productos mb-4">
         <!--CONTENEDOR DE CARDS-->
         <div class="container-cards">
             
             <!--CARD-->
-            <div class="card costum-card" style="width: 200px;">
-                <img src="images/moto1.jpg" class="card-img-top shadow-sm" alt="moto">
-                <div class="card-body pb-4">
-                  <h5 class="card-title">Moto</h5>
-                  <p class="card-text">$Precio</p>
-                  <div class="d-flex justify-content-end w-100">
-                      <a href="#" class="boton-principal align-bottom">Comprar</a>
-                  </div>
-                </div>
-            </div>
-
-            <div class="card costum-card" style="width: 200px;">
-                <img src="images/moto1.jpg" class="card-img-top shadow-sm" alt="moto">
-                <div class="card-body pb-4">
-                  <h5 class="card-title">Moto</h5>
-                  <p class="card-text">$Precio</p>
-                  <div class="d-flex justify-content-end w-100">
-                      <a href="#" class="boton-principal align-bottom">Comprar</a>
-                  </div>
-                </div>
-            </div>
-
-            <div class="card costum-card" style="width: 200px;">
-                <img src="images/moto1.jpg" class="card-img-top shadow-sm" alt="moto">
-                <div class="card-body pb-4">
-                  <h5 class="card-title">Moto</h5>
-                  <p class="card-text">$Precio</p>
-                  <div class="d-flex justify-content-end w-100">
-                      <a href="#" class="boton-principal align-bottom">Comprar</a>
-                  </div>
-                </div>
-            </div>
-
             <div class="card costum-card" style="width: 200px;">
                 <img src="images/moto1.jpg" class="card-img-top shadow-sm" alt="moto">
                 <div class="card-body pb-4">
@@ -140,7 +81,3 @@
 
     <!--FOOTER-->
     @include('footer')
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
-</html>
