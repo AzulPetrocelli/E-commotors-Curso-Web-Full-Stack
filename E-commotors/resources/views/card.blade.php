4e7f0ddcae7cc51ebb2219f8c4cee389440e7ccf
AzulPetrocelli <azulpetrocelli81@gmail.com>
2024-12-23 11:43:03 -0300

    Terminada la vista de las cards

--- E-commotors/resources/views/card.blade.php
+++ E-commotors/resources/views/card.blade.php
@@ -1,18 +1,20 @@
 @include('header')
 
-<section class="d-flex m-5 py-5">
-    <div>
+<section class="d-flex mt-5 mx-2 pt-5 gap-3 container-card-view">
+    <div class="container-images-card-view">
         <img src="images/moto3.jpg" class="card-image" alt="Moto">
-        <div></div>
     </div>
     <div>
         <h1 class="jaro fs-1">Nombre</h1>
         <h2 class="jaro fs-2">Descripcion</h2>
-        <p class="varela-round-regular">Lorem ipsum dolor sit amet consectetur adipisicing elit. In fugit, odit esse aliquam facere quis doloremque, pariatur ea iste, excepturi id nobis maxime at commodi eligendi ad. Ratione, natus consequuntur.</p>
+        <p class="varela-round-regular">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus ex quisquam debitis voluptates, maiores nesciunt. Temporibus eius rerum aut, excepturi debitis officiis quam doloribus neque, nobis molestias laboriosam natus cumque.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus ex quisquam debitis voluptates, maiores nesciunt. Temporibus eius rerum aut, excepturi debitis officiis quam doloribus neque, nobis molestias laboriosam natus cumque.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In fugit, odit esse aliquam facere quis doloremque, pariatur ea iste, excepturi id nobis maxime at commodi eligendi ad. Ratione, natus consequuntur.</p>
         <p class="jaro fs-2">Precio $700.000</p>
-        <div>
-            <a href="#" class="boton-principal">Enviar Mensaje</a>
-            <a href="#" class="boton-principal">Comprar</a>
+        <div class="d-flex justify-content-end">
+            <a href="#" class="boton-principal m-2">Enviar Mensaje</a>
+            <a href="#" class="boton-principal m-2">Comprar</a>
         </div>
     </div>
 </section>
