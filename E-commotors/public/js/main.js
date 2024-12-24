34df1358ca94a7f6b1e286b6de464cdb29d3dfe3
AzulPetrocelli <azulpetrocelli81@gmail.com>
2024-12-24 12:43:18 -0300

    Ya aparece y desaparece el aside

--- E-commotors/public/js/main.js
+++ E-commotors/public/js/main.js
@@ -1,13 +1,15 @@
 
 //PARA QUE APAREZCA Y DESAPAREZCA EL ASIDE DEL FILTER
-var botonFiltrar = document.querySelector(".filtrar")
+var botonFiltrar = document.querySelectorAll(".filtrar")
 
-botonFiltrar.addEventListener("click", () => {
-  var contenedorAside = document.querySelector(".filter-sidebar")
-  
-  if(contenedorAside.classList.contains("aside-oculto")){
-      contenedorAside.classList.remove("aside-oculto")
-  } else {
-      contenedorAside.classList.add("aside-oculto")
-  }
+botonFiltrar.forEach(boton => {
+    boton.addEventListener("click", () => {
+        var contenedorAside = document.querySelector(".filter-sidebar")
+        
+        if(contenedorAside.classList.contains("aside-oculto")){
+            contenedorAside.classList.remove("aside-oculto")
+        } else {
+            contenedorAside.classList.add("aside-oculto")
+        }
+      })
 })
