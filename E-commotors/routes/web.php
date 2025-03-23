85c8e16ef9c8786b2620cd6cc7f7efb7e9f52e16
Azul Petrocelli <azulpetrocelli81@gmail.com>
2025-03-23 00:04:19 -0300

    ordene los routes y los agrupe segun el controler al que llamen

--- E-commotors/routes/web.php
+++ E-commotors/routes/web.php
@@ -11,16 +11,15 @@ use App\Http\Controllers\MensajeController;
 use App\Http\Controllers\RepuestosController;
 use App\Http\Controllers\AccesoriosController;
 
+// Rutas sin controlador
 Route::get('/main', function () {
     $motosAleatorias = Moto::inRandomOrder()->take(3)->get();
     return view('main',['motosAleatorias' => $motosAleatorias]);
-}) -> name("/main");
-
+})->name('/main');
 
 Route::get('/card', function () {
     return view('card');
 });
-
 
 Route::get('/', function () {
     return view('welcome');
@@ -28,112 +27,55 @@ Route::get('/', function () {
 
 Route::get('/administracion', function () {
     return view('admin');
-}) -> name('admin');
+})->name('admin');
 
- //ROUTE -> vista de enviar un mensaje
-Route::get('/message', [MensajeController::class, 'index'])->name('message');
-
- //ROUTE -> vista de admin
-Route::get('admin',function(){
+Route::get('admin', function () {
     return view('admin');
-})->middleware("auth");
+})->middleware('auth');
 
-/* Route::get('/productos-motos', function () {
-    return view('productos');
-}); */
+// Rutas AuthController
+Route::post     ('/login',                          [AuthController::class,        'login'])                    ->name('log');
 
- //ROUTE -> vista de Productos/motos
-Route::get('/productos-motos', [MotosController::class, 'index'])->name('productos-motos');
+// Rutas AdminController
+Route::get      ('/accion-mensaje',                 [AdminController::class,       'mensajes'])                 ->name('accion-mensaje')->middleware('auth');
+Route::get      ('/accion-mensaje-sin_responder',   [AdminController::class,       'mensajesSinResponder'])     ->name('accion-mensaje-sin-responder');
 
-Route::get('/productos-accesorios', [AccesoriosController::class, 'index'])->name('productos-accesorios');
+// Rutas MensajeController
+Route::get      ('/message',                        [MensajeController::class,      'index'])       ->name('message');
+Route::post     ('/enviar-mensaje',                 [MensajeController::class,      'store'])       ->name('mensaje.store');
+Route::post     ('/admin/mensajes/{id}/responder',  [MensajeController::class,      'responder'])   ->name('admin-mensajes-responder');
 
+// Rutas MotosController
+Route::get      ('/accion-moto',                    [MotosController::class,        'showItems'])   ->name('accionMoto')->middleware('auth');
+Route::get      ('/productos-motos',                [MotosController::class,        'index'])       ->name('productos-motos');
+Route::get      ('/card/{id}',                      [MotosController::class,        'show'])        ->name('motos.show');
+Route::get      ('/accion-moto/search',             [MotosController::class,        'busqueda'])    ->name('busqueda.moto');
+Route::get      ('/accion-moto/agregar',            [MotosController::class,        'create'])      ->name('accionMotoCreate');
+Route::post     ('/accion-moto/agregar',            [MotosController::class,        'store'])       ->name('agregarMoto');
+Route::put      ('/motos/{id}',                     [MotosController::class,        'update'])      ->name('editarMoto');
+Route::delete   ('/motos/{id}',                     [MotosController::class,        'destroy'])     ->name('motos.destroy');
+Route::post     ('/productos-motos#',               [MotosController::class,        'filtrar']);
 
-// Ruta para mostrar card de una moto específica
-Route::get('/card/{id}', [MotosController::class, 'show'])->name('motos.show');
+// Rutas AccesoriosController
+Route::get      ('/accion-accesorio',               [AccesoriosController::class,   'showItems'])           ->name('accionAccesorio')->middleware('auth');
+Route::get      ('/productos-accesorios',           [AccesoriosController::class,   'index'])               ->name('productos-accesorios');
+Route::get      ('/cardAccesorios/{id}',            [AccesoriosController::class,   'show'])                ->name('accesorios.show');
+Route::get      ('/accesorios/tipo/{id}',           [AccesoriosController::class,   'filtrarPorTipo'])      ->name('accesorios.filtrar');
+Route::get      ('/accion-accesorios',              [AccesoriosController::class,   'busqueda'])            ->name('busqueda.accesorio');
+Route::delete   ('/accesorios/{id}',                [AccesoriosController::class,   'destroy'])             ->name('eliminarAccesorio');
+Route::post     ('/accion-accesorio/agregar',       [AccesoriosController::class,   'store'])               ->name('agregarAccesorio');
+Route::put      ('/accesorios/{id}',                [AccesoriosController::class,   'update'])              ->name('editarAccesorio');
 
-Route::get('/cardAccesorios/{id}', [AccesoriosController::class, 'show'])->name('accesorios.show');
+// Rutas RepuestosController
+Route::get      ('/accion-repuesto',                    [RepuestosController::class,       'showItems'])        ->name('accionRepuesto')->middleware('auth');
+Route::get      ('/repuestos/tipo/{id}',                [RepuestosController::class,       'filtrarPorTipo'])   ->name('repuestos.filtrar');
+Route::get      ('/productos-repuestos',                [RepuestosController::class,       'index'])            ->name('repuestos.index');
+Route::get      ('/productos-repuestos/{id}',           [RepuestosController::class,       'show'])             ->name('repuesto.show');
+Route::get      ('/productos-repuestos/filtrar/{id}',   [RepuestosController::class,       'filtrarPorTipo'])   ->name('repuestos.filtrarPorTipo');
+Route::get      ('/productos-repuestos/buscar',         [RepuestosController::class,       'busqueda'])         ->name('repuestos.busqueda');
 
-Route::get('/accesorios/tipo/{id}', [AccesoriosController::class, 'filtrarPorTipo'])->name('accesorios.filtrar');
-
-Route::get('/repuestos/tipo/{id}', [RepuestosController::class, 'filtrarPorTipo'])->name('repuestos.filtrar');
-
-
-//Route -> muestro items
-Route::get('/accion-moto', [MotosController::class, 'showItems'])->name('accionMoto')->middleware("auth");
-
-//Route -> busco items
-Route::get('/accion-moto/search',[MotosController::class,'busqueda'])->name('busqueda.moto');
-
-//Route -> muestro items
-Route::get('/accion-accesorios',[AccesoriosController::class,'busqueda'])->name('busqueda.accesorio');
-
-Route::delete('/accesorios/{id}', [AccesoriosController::class, 'destroy'])->name('eliminarAccesorio');
-
-//Route -> Agregar Producto
-Route::post('/accion-accesorio/agregar', [AccesoriosController::class, 'store'])->name('agregarAccesorio');
-
-//Route -> form para agregar un producto
-Route::get('/accion-moto/agregar', [MotosController::class, 'create'])->name('accionMotoCreate');
-
-//Route -> Agregar Producto
-Route::post('/accion-moto/agregar', [MotosController::class, 'store'])->name('agregarMoto');
-
-//Route -> Editar Producto
-Route::put('/motos/{id}', [MotosController::class, 'update'])->name('editarMoto');
-
-Route::put('/accesorios/{id}', [AccesoriosController::class, 'update'])->name('editarAccesorio');
-
-Route::delete('/motos/{id}', [MotosController::class, 'destroy'])->name('motos.destroy');
-
-Route::put('/motos/{id}', [MotosController::class, 'update'])->name('editarMoto');
-
-
-//Route -> muestro items
-Route::get('/accion-accesorio',[AccesoriosController::class,'showItems'])->name('accionAccesorio')->middleware("auth");
-
-//Route -> accion repuesto
-Route::get('/accion-repuesto',[RepuestosController::class,'showItems'])->name('accionRepuesto')->middleware("auth");
-
-//Route -> vista de las motos filtradas
-Route::post('/productos-motos#', [MotosController::class, 'filtrar']);
-
-
-//ROUTE -> vista de mensajes recibidos
-Route::get('/accion-mensaje',[AdminController::class,'mensajes']) -> name('accion-mensaje')->middleware("auth");
-
-//
-Route::get('/accion-mensaje-sin_responder',[AdminController::class,'mensajesSinResponder']) -> name('accion-mensaje-sin-responder');
-
-// ROUTE ->  responder mensaje
-Route::post('/admin/mensajes/{id}/responder', [MensajeController::class, 'responder'])->name('admin-mensajes-responder');
-
-
-
-
-//-----------------------------------------------------------------------------------------------------------------------
-// ROUTES DE FOOTER
-
-//ROUTE -> vista de /terminos-y-condiciones (archivo terminos)
-Route::get('/terminos-y-condiciones',[FooterController::class,"terminosYcondiciones"]) -> name('terminosYcondiciones');
-
-//ROUTE -> vista de /politicas-de-envio (archivo politicas)
-Route::get('/politicas-de-envio',[FooterController::class,"politicasDeEnvio"]) -> name('politicas');
-
-//ROUTE -> vista de /locales (archivo locale)
-Route::get('/locales',[FooterController::class,"locales"]) -> name('locales');
-
-// ROUTE -> DESCARGA DE PDF CONDICIONES
-Route::get('/descargar-devoluciones', [FooterController::class, 'descargarDevoluciones'])->name('descargarPDF');
-
-// ROUTE ENVIAR MENSAJE
-Route::post('/enviar-mensaje', [MensajeController::class, 'store'])->name('mensaje.store');
-
-//ROUTE LOGUIN
-Route::post('/login', [AuthController::class, 'login'])->name('log');
-
-
-//ROUTES PARA REPUESTOS
-Route::get('/productos-repuestos', [RepuestosController::class, 'index'])->name('repuestos.index');
-Route::get('/productos-repuestos/{id}', [RepuestosController::class, 'show'])->name('repuesto.show');
-Route::get('/productos-repuestos/filtrar/{id}', [RepuestosController::class, 'filtrarPorTipo'])->name('repuestos.filtrarPorTipo');
-Route::get('/productos-repuestos/buscar', [RepuestosController::class, 'busqueda'])->name('repuestos.busqueda');
+// Rutas FooterController
+Route::get      ('/terminos-y-condiciones',         [FooterController::class,       'terminosYcondiciones'])    ->name('terminosYcondiciones');
+Route::get      ('/politicas-de-envio',             [FooterController::class,       'politicasDeEnvio'])        ->name('politicas');
+Route::get      ('/locales',                        [FooterController::class,       'locales'])                 ->name('locales');
+Route::get      ('/descargar-devoluciones',         [FooterController::class,       'descargarDevoluciones'])   ->name('descargarPDF');
